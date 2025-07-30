a5ccbf69bb605fc0df09c57fe25a3d7497f6b96e
ISP-ITE02 <ISP-HRE05@ISP-ITE02.isanpalm.local>
2025-07-30 12:25:53 +0700

    เริ่มต้นโปรเจกต์

---
+++ app/Livewire/ธindex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ธindex extends Component
+{
+    public function render()
+    {
+        return view('livewire.ธindex');
+    }
+}
